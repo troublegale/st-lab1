e672de552339f2683f033f8ec4ff74c86d446945
tg <eat.some.trouble@gmail.com>
2025-02-14 06:07:26 +0300

    task 3

---
+++ src/main/kotlin/TaskThree.kt
@@ -0,0 +1,80 @@
+package itmo.tg
+
+import itmo.tg.IntelligentBeing.Companion.information
+
+/*
+Возможно, ее сообщение привлекло бы больше внимания, если бы было известно,
+что люди были третьими по уровню интеллекта существами на планете Земля,
+а не (как полагало большинство независимых обозревателей) вторыми.
+ */
+
+enum class Intelligence {
+    NOT_SO_SMART, SMART, THE_SMARTEST
+}
+
+interface IntelligentBeing {
+    companion object {
+        var information: String? = null
+    }
+    fun observe(being: IntelligentBeing): Intelligence
+    fun hear(message: String) {
+        information = message
+    }
+    fun speak() = information
+}
+
+class Man : IntelligentBeing {
+    override fun observe(being: IntelligentBeing): Intelligence {
+        return when (being) {
+            is Man -> Intelligence.SMART
+            is Woman -> Intelligence.NOT_SO_SMART
+            is God -> Intelligence.THE_SMARTEST
+            else -> throw IllegalArgumentException(
+                "Can only observe men, women and gods")
+        }
+    }
+}
+
+class Woman : IntelligentBeing {
+    override fun observe(being: IntelligentBeing): Intelligence {
+        return when (being) {
+            is Man -> Intelligence.NOT_SO_SMART
+            is Woman -> Intelligence.SMART
+            is God -> Intelligence.THE_SMARTEST
+            else -> throw IllegalArgumentException(
+                "Can only observe men, women and gods")
+        }
+    }
+    fun bringMessage(message: String?): String {
+        return if (message == null) "I have nothing to say."
+        else "I bring a very important message: $message"
+    }
+}
+
+class God : IntelligentBeing {
+    override fun observe(being: IntelligentBeing): Intelligence {
+        return when (being) {
+            is Man -> Intelligence.NOT_SO_SMART
+            is Woman -> Intelligence.NOT_SO_SMART
+            is God -> Intelligence.SMART
+            else -> throw IllegalArgumentException(
+                "Can only observe men, women and gods")
+        }
+    }
+}
+
+class Monkey : IntelligentBeing {
+    override fun observe(being: IntelligentBeing): Intelligence {
+        return when (being) {
+            is Man -> Intelligence.NOT_SO_SMART
+            is Woman -> Intelligence.NOT_SO_SMART
+            is God -> Intelligence.NOT_SO_SMART
+            else -> Intelligence.THE_SMARTEST
+        }
+    }
+}
+
+fun getObservers(): List<IntelligentBeing> {
+    return listOf(Man(), Man(), Man(),
+        Woman(), Woman(), God(), Monkey())
+}
---
+++ src/test/kotlin/TaskThreeTests.kt
@@ -0,0 +1,69 @@
+import itmo.tg.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class TaskThreeTests {
+
+    @Test
+    fun `When she has nothing to say, she says so`() {
+        val she = Woman()
+        assertEquals("I have nothing to say.", she.bringMessage(null))
+    }
+
+    @Test
+    fun `When she has something to say, she says it`() {
+        val she = Woman()
+        assertEquals("I bring a very important message: Amogus!",
+            she.bringMessage("Amogus!"))
+    }
+
+    @Test
+    fun `She is smarter than men`() {
+        val she = Woman()
+        val someMan = Man()
+        assertEquals(Intelligence.SMART, she.observe(she))
+        assertEquals(Intelligence.NOT_SO_SMART, she.observe(someMan))
+    }
+
+    @Test
+    fun `Most observers think that men are the second smartest beings`() {
+        val observers = getObservers()
+        val she = Woman()
+        val herScore = observers.stream().filter {
+            o -> o.observe(she) == Intelligence.SMART }.count()
+        val someMan = Man()
+        val someMansScore = observers.stream().filter {
+            o -> o.observe(someMan) == Intelligence.SMART }.count()
+        assert(herScore < someMansScore)
+    }
+
+    @Test
+    fun `Observers only observe men, women and gods`() {
+        val man = Man()
+        assertThrows<IllegalArgumentException> { man.observe(Monkey()) }
+    }
+
+    @ParameterizedTest
+    @MethodSource("testMessages")
+    fun `They could have heard her`(message: String) {
+        val she = Woman()
+        val observers = getObservers()
+        observers.forEach { o -> o.hear(she.bringMessage(message)) }
+        assert(observers.all { o -> o.speak().equals(
+            "I bring a very important message: $message") })
+    }
+
+    companion object {
+        @JvmStatic
+        fun testMessages() = listOf(
+            Arguments.of("Amogus!"),
+            Arguments.of("I am smart!"),
+            Arguments.of("Make America Great Again!")
+        )
+    }
+
+}
